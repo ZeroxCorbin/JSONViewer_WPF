c975ae1036cd416c38b7b4e33b32dcc60e10065b
Jack <jack@jackb.me>
2024-11-03 22:22:43 -0500

    Update project files and improve XAML formatting
    Updated `JSONViewer_WPF.csproj` to target `net8.0-windows7.0`, set `LangVersion` to `latest`, enable nullable reference types, and add a project reference to `HexView.Wpf`. Updated `JSONViewer_WPF.sln` to include `HexView.Wpf` project and its configurations. Reformatted `JsonViewer.xaml` for better readability and consistency, and added `HexPropertyTemplate` to `JPropertyDataTemplateSelector`. Updated `JPropertyDataTemplateSelector.cs` to include new `HexPropertyTemplate` property and case statements for `JTokenType` values. Changed `HexView.Wpf.csproj` to target `net8.0-windows7.0`.

--- TemplateSelectors/JPropertyDataTemplateSelector.cs
+++ TemplateSelectors/JPropertyDataTemplateSelector.cs
@@ -11,6 +11,8 @@ namespace JSONViewer_WPF.TemplateSelectors
         public DataTemplate ArrayPropertyTemplate { get; set; }
         public DataTemplate ObjectPropertyTemplate { get; set; }
 
+        public DataTemplate HexPropertyTemplate { get; set; }
+
         public override DataTemplate SelectTemplate(object item, DependencyObject container)
         {
             if(item == null)
@@ -30,6 +32,38 @@ namespace JSONViewer_WPF.TemplateSelectors
                         return frameworkElement.FindResource("ObjectPropertyTemplate") as DataTemplate;
                     case JTokenType.Array:
                         return frameworkElement.FindResource("ArrayPropertyTemplate") as DataTemplate;
+                    case JTokenType.None:
+                        break;
+                    case JTokenType.Constructor:
+                        break;
+                    case JTokenType.Property:
+                        break;
+                    case JTokenType.Comment:
+                        break;
+                    case JTokenType.Integer:
+                        break;
+                    case JTokenType.Float:
+                        break;
+                    case JTokenType.String:
+                        break;
+                    case JTokenType.Boolean:
+                        break;
+                    case JTokenType.Null:
+                        break;
+                    case JTokenType.Undefined:
+                        break;
+                    case JTokenType.Date:
+                        break;
+                    case JTokenType.Raw:
+                        break;
+                    case JTokenType.Bytes:
+                        break;
+                    case JTokenType.Guid:
+                        break;
+                    case JTokenType.Uri:
+                        break;
+                    case JTokenType.TimeSpan:
+                        break;
                     default:
                         return frameworkElement.FindResource("PrimitivePropertyTemplate") as DataTemplate;
 
